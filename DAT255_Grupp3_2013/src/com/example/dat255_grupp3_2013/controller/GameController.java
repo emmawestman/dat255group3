bda264e782adb61347799457eb47be86ac342743
SaraWallander <sarawallander93@gmail.com>
2013-09-19 16:09:19 +0200

    added some extend and implement and autogenerated the needed methods

--- DAT255_Grupp3_2013/src/com/example/dat255_grupp3_2013/controller/GameController.java
+++ DAT255_Grupp3_2013/src/com/example/dat255_grupp3_2013/controller/GameController.java
@@ -1,7 +1,15 @@
 package com.example.dat255_grupp3_2013.controller;
 
-public class GameController {
+import com.badlogic.gdx.Game;
+
+public class GameController extends Game {
 	public GameController () {
+		
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
 		
 	}
 }
--- DAT255_Grupp3_2013/src/com/example/dat255_grupp3_2013/controller/InGameController.java
+++ DAT255_Grupp3_2013/src/com/example/dat255_grupp3_2013/controller/InGameController.java
@@ -1,7 +1,51 @@
 package com.example.dat255_grupp3_2013.controller;
 
-public class InGameController {
+import com.badlogic.gdx.Screen;
+
+public class InGameController implements Screen {
 	public InGameController () {
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
 		
 	}
 }
