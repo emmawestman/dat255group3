b6f2e23f2c558de685b3ba8b4b624e642dcbaea2
SaraWallander <sarawallander93@gmail.com>
2013-09-20 21:07:20 +0200

    now, when you touch the red screen, it becomes yellow for a short while, yey progress

--- dat255_group3/src/com/dat255_group3/controller/InGameController.java
+++ dat255_group3/src/com/dat255_group3/controller/InGameController.java
@@ -18,52 +18,21 @@ public class InGameController implements Screen{
 	private InGame inGame;
 	private InGameView inGameView;
 	
-	//for test
-	/*
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	*/
 	
 	public InGameController(MyGdxGameController myGdxGameController){
 		this.myGdxGameController = myGdxGameController;
 		this.inGameView = new InGameView();
 		this.inGame = new InGame();
 		
-		//for test
-		/*
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/eightbitPC.gif"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-	*/
-		
 		
 	}
 	
 	@Override
 	public void render(float delta) {
+		//show a green screen
 		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		/*
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-		*/
 	}
 
 	@Override
@@ -74,7 +43,9 @@ public class InGameController implements Screen{
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
+		//show a yellow screen
+		Gdx.gl.glClearColor(1, 1, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 	}
 
--- dat255_group3/src/com/dat255_group3/controller/MyGdxGameController.java
+++ dat255_group3/src/com/dat255_group3/controller/MyGdxGameController.java
@@ -3,12 +3,6 @@ package com.dat255_group3.controller;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.dat255_group3.model.MyGdxGame;
 
 
@@ -16,12 +10,6 @@ public class MyGdxGameController extends Game {
 	private MyGdxGame myGdxGame;
 	private InGameController inGameController;
 	private PlayerController playerController;
-	/*
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	*/
 	
 	@Override
 	public void create() {
@@ -33,36 +21,17 @@ public class MyGdxGameController extends Game {
 		//go to the first screen
 		setScreen(new InGameController(this));
 		
-		/*
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/eightbitPC.gif"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-		*/
 	}
 
 	@Override
 	public void dispose() {
-		/*
-		batch.dispose();
-		texture.dispose();
-		*/
+		
 	}
 
 	@Override
 	public void render() {	
-		
+		//show a red screen
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
@@ -70,14 +39,6 @@ public class MyGdxGameController extends Game {
 			setScreen(new InGameController(this));
 		}
 		
-		
-		//Gdx.gl.glClearColor(1, 1, 1, 1);
-		//Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		//batch.setProjectionMatrix(camera.combined);
-		//batch.begin();
-		//sprite.draw(batch);
-		//batch.end();
 		
 	}
 
