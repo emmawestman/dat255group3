d5bd0ad75a0af79b41379d213d08466510b8ddcf
SaraWallander <sarawallander93@gmail.com>
2013-09-23 16:01:57 +0200

    added some variables and a method to enable physics and create a solid ground

--- dat255_group3/src/com/dat255_group3/controller/WorldController.java
+++ dat255_group3/src/com/dat255_group3/controller/WorldController.java
@@ -1,5 +1,11 @@
 package com.dat255_group3.controller;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.dat255_group3.model.World;
 import com.dat255_group3.view.WorldView;
 
@@ -8,11 +14,42 @@ public class WorldController {
 	private InGameController inGameController;
 	private World world;
 	private WorldView worldView;
+	private Vector2 gravity;
+	final boolean doSleep;
+	private Body groundBody;
+	private static com.badlogic.gdx.physics.box2d.World physicsWorld;
 	
 	public WorldController(InGameController inGameController){
 		this.inGameController = inGameController;
 		this.world = new World();
 		this.worldView = new WorldView();
+		
+		//create the physics world
+		this.setGravity(new Vector2(0.0f, 9.82f));
+		this.doSleep = true;
+		this.physicsWorld = new com.badlogic.gdx.physics.box2d.World(gravity, doSleep);
+		
+	}
+	
+	//method to create a temporary ground for a character to stand on until we can read from a map. 
+	//May also be used in the future to create "solid ground" bodies (obstacles) 
+	//(but then we should probably move it move it)
+	public static void createGround(final Vector2 pos, final Vector2 size, final float friction, final float restitution) {
+		PolygonShape polygonShape = new PolygonShape();
+		polygonShape.setAsBox(size.x, size.y);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = polygonShape;
+		fixtureDef.friction = friction;
+		fixtureDef.density = 1f;
+		fixtureDef.restitution = restitution;
+
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set(pos);
+		bodyDef.type = BodyType.StaticBody;
+		bodyDef.fixedRotation = true;
+
+		Body body = physicsWorld.createBody(bodyDef);
+		body.createFixture(fixtureDef);
 	}
 
 	public World getWorld() {
@@ -21,6 +58,14 @@ public class WorldController {
 
 	public WorldView getWorldView() {
 		return worldView;
+	}
+
+	public Vector2 getGravity() {
+		return gravity;
+	}
+
+	public void setGravity(Vector2 gravity) {
+		this.gravity = gravity;
 	}
 	
 	
