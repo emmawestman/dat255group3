da6f8a1155c276e0c9333c6597ee13470ef7ab55
Björn <theturnipman@gmail.com>
2013-09-24 11:31:39 +0200

    MainActivity now logs resolution.

--- dat255_group3-android/src/com/dat255_group3/MainActivity.java
+++ dat255_group3-android/src/com/dat255_group3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dat255_group3;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -10,7 +13,15 @@ public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
         
+        Log.d("Viking","Width: "+width+" Height: "+height);
+
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useGL20 = false;
         
