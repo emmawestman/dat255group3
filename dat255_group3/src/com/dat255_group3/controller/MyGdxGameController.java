b34acff9f57b88aecf4896fa8387c8539a57a56c
Björn <theturnipman@gmail.com>
2013-09-26 15:55:35 +0200

    Created getters and setters from width and height.

--- dat255_group3/src/com/dat255_group3/controller/MyGdxGameController.java
+++ dat255_group3/src/com/dat255_group3/controller/MyGdxGameController.java
@@ -1,6 +1,7 @@
 package com.dat255_group3.controller;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.dat255_group3.model.MyGdxGame;
 
 
@@ -39,5 +40,13 @@ public class MyGdxGameController extends Game {
 
 	@Override
 	public void resume() {
+	}
+	
+	public void setHeight(int height){
+		Gdx.app.log("Viking", "Height: "+height);
+	}
+
+	public void setWidth(int width) {
+		Gdx.app.log("Viking", "Width: "+width);
 	}
 }
