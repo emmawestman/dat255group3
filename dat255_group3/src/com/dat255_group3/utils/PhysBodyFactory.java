979c951719e02d0e5ab58144e0d5d0a6563716d2
SaraWallander <sarawallander93@gmail.com>
2013-09-26 14:06:00 +0200

    added method to create the physics for the character

--- dat255_group3/src/com/dat255_group3/utils/PhysBodyFactory.java
+++ dat255_group3/src/com/dat255_group3/utils/PhysBodyFactory.java
@@ -11,6 +11,32 @@ import com.badlogic.gdx.physics.box2d.World;
 public class PhysBodyFactory {
 
 	
+	public static Body createCharacter(World physWorld, Vector2 pos, Vector2 size) {
+		PolygonShape shape;
+		FixtureDef fixtureDef = new FixtureDef();
+		BodyDef bodyDef = new BodyDef();
+		Body body = null;
+
+
+		shape = new PolygonShape();
+		shape.setAsBox(size.x/2, size.y/2);
+
+		fixtureDef.shape = shape;
+		fixtureDef.density = 1f;
+		fixtureDef.friction = 0.8f;
+		fixtureDef.restitution = 0f;
+
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.fixedRotation = true;
+		bodyDef.position.set(pos.x, pos.y);
+
+		body = physWorld.createBody(bodyDef);
+		body.createFixture(fixtureDef);
+
+		return body;
+	}
+	
+	
 	public static void addSolidGround(final Vector2 pos, final Vector2 size, final float friction, final float restitution, World physWorld) {
 		PolygonShape polygonShape = new PolygonShape();
 		polygonShape.setAsBox(size.x, size.y);
