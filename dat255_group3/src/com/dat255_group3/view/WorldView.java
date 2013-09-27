7bb3afda8a976a0ebe42cbf939fb0b7eb3a12483
Björn <theturnipman@gmail.com>
2013-09-27 10:06:11 +0200

    Added javadoc.

--- dat255_group3/src/com/dat255_group3/view/WorldView.java
+++ dat255_group3/src/com/dat255_group3/view/WorldView.java
@@ -3,16 +3,26 @@ package com.dat255_group3.view;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+/** 
+ * @author The Hans-Gunnar Crew
+ *
+ */
 public class WorldView {
 	
 	
 	
 	
 	
+	/**
+	 * 
+	 */
 	public WorldView () {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void render() {
 		//Draws characterview
 		
