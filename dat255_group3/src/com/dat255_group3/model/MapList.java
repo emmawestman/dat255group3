ac21dfe2db15f7e53b78a3b9f00edf5ef0aecd0f
Soya93 <s.n1993@hotmail.com>
2013-09-27 10:03:06 +0200

    Added javadocs for some modelclasses

--- dat255_group3/src/com/dat255_group3/model/MapList.java
+++ dat255_group3/src/com/dat255_group3/model/MapList.java
@@ -2,13 +2,26 @@ package com.dat255_group3.model;
 
 import java.util.ArrayList;
 
+/**
+ * A class describing a list for maps.
+ * @author Grupp 3
+ *
+ */
 public class MapList {
 	private ArrayList<Position> mapList;
 
+	/**
+	 * Construcs a MapList.
+	 */
 	public MapList() {
 		mapList = new ArrayList<Position>();		
 	}
 
+	/**
+	 * A method which returns the list of maps as it is invoked.
+	 * @return
+	 * 		The maplist
+	 */
 	public ArrayList<Position> getMapList() {
 		return mapList;
 	}
