8f1150963b86765c573045966772f9ea978b38b1
EmmaWestman <eewestman@gmail.com>
2013-10-01 10:15:56 +0200

    Added pysical representation of objects in the world

--- dat255_group3/src/com/dat255_group3/controller/WorldController.java
+++ dat255_group3/src/com/dat255_group3/controller/WorldController.java
@@ -1,7 +1,11 @@
 package com.dat255_group3.controller;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.dat255_group3.model.MapList;
+import com.dat255_group3.model.Position;
 import com.dat255_group3.model.World;
 import com.dat255_group3.utils.PhysBodyFactory;
 import com.dat255_group3.utils.WorldUtil;
@@ -25,18 +29,34 @@ public class WorldController {
 		this.world = new World();
 		this.worldView = new WorldView();
 		this.characterController = new CharacterController(this);
-		//this.worldUtil = new WorldUtil(inGameController.getMap());
+		this.worldUtil = new WorldUtil(inGameController.getMap());
 
 		//create the physics world
 		this.setGravity(new Vector2(0.0f, -10f));
 		this.doSleep = true;
 		this.physicsWorld = new com.badlogic.gdx.physics.box2d.World(gravity, doSleep);
-		// TODO create the ground
-		groundBody = PhysBodyFactory.addSolidGround(new Vector2(240f, 0f), new Vector2(240f,10f), 0.8f, 0f, this.physicsWorld);
+		
+		
+		//create the ground
+		ArrayList<Position> groundList = worldUtil.getGroundList().getMapList();
+		for(int i=0; i<groundList.size(); i++) {
+			PhysBodyFactory.addSolidGround(new Vector2(groundList.get(i).getX(),groundList.get(i).getY()),
+					worldUtil.getTileSize(), 0.8f, 0f, this.physicsWorld);
+		}
+		
+		//create obstacles
+		ArrayList<Position> obstacleList = worldUtil.getGroundList().getMapList();
+		for(int i=0; i<obstacleList.size(); i++) {
+			PhysBodyFactory.addSolidGround(new Vector2(obstacleList.get(i).getX(),obstacleList.get(i).getY()),
+					worldUtil.getTileSize(), 0.8f, 0f, this.physicsWorld);
+		}
+		
+		
+//		groundBody = PhysBodyFactory.addSolidGround(new Vector2(240f, 0f), new Vector2(240f,10f), 0.8f, 0f, this.physicsWorld);
 		this.worldView = new WorldView();
 		this.characterController = new CharacterController(this);
 		//create character body
-		this.charBody = PhysBodyFactory.createCharacter(physicsWorld, new Vector2(240f, 100f), new Vector2(5f, 10f));
+		this.charBody = PhysBodyFactory.createCharacter(physicsWorld, new Vector2(worldUtil.getStartPos().x, worldUtil.getStartPos().y-worldUtil.getTileSize().y), new Vector2(5f, 10f));
 	}
 	
 
--- dat255_group3/src/com/dat255_group3/utils/WorldUtil.java
+++ dat255_group3/src/com/dat255_group3/utils/WorldUtil.java
@@ -3,27 +3,39 @@ package com.dat255_group3.utils;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Vector2;
 import com.dat255_group3.model.MapList;
 import com.dat255_group3.model.Position;
 
 public class WorldUtil {
 
-	private Position startPos;
+	private Vector2 startPos;
 	private TiledMap map;
 	private MapList groundList;
 	private MapList obstacleList;
 	private MapList finishLineList;
+	private Vector2 tileSize;
 
 	public WorldUtil(TiledMap tiledMap) {
 		this.map = tiledMap;
 		groundList = new MapList();
 		obstacleList = new MapList();
 		finishLineList = new MapList();
+		setTileSize();
 		addToLists();
 
 	}
+	
+	public void setTileSize() {
+		TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get(0);
+		tileSize = new Vector2(layer.getTileHeight(), layer.getTileWidth());
+	}
+	
+	public Vector2 getTileSize() {
+		return tileSize;
+	}
 
-	public Position getStartPos() {
+	public Vector2 getStartPos() {
 		return startPos;
 	}
 
@@ -38,29 +50,38 @@ public class WorldUtil {
 	public MapList getFinishLineList() {
 		return finishLineList;
 	}
+	
+	public void placePhysObject() {
+		
+	}
 
 	private void addToLists() {
 		for(int i=0; i<map.getLayers().getCount(); i++) {
 			TiledMapTileLayer currentLayer = (TiledMapTileLayer)map.getLayers().get(i);
 			if(currentLayer.getName().equalsIgnoreCase("Solids")) {
-				for(int x=0; x<currentLayer.getHeight(); x++) {
-					for(int y=0; y<currentLayer.getWidth(); y++) {
-						TiledMapTile tile = currentLayer.getCell(x, y).getTile();
-						if(tile.getId()==1) {
-							groundList.getMapList().add(new Position(x,y));
-						}else if(tile.getId()==2) {
-							obstacleList.getMapList().add(new Position(x,y));
+				for(int x=0; x<currentLayer.getWidth(); x++) {
+					for(int y=0; y<currentLayer.getHeight(); y++) {
+						if(currentLayer.getCell(x, y) != null) {
+							TiledMapTile tile = currentLayer.getCell(x, y).getTile();
+							if(tile.getProperties().containsKey("Ground")) {
+								groundList.getMapList().add(new Position(x,currentLayer.getHeight()-(y+1)));
+							}else if(tile.getProperties().containsKey("Obstacle")) {
+								obstacleList.getMapList().add(new Position(x,currentLayer.getHeight()-(y+1)));
+							}
 						}
 					}
+
 				}
 			}else if(currentLayer.getName().equalsIgnoreCase("Positions")) {
-				for(int x=0; x<currentLayer.getHeight(); x++) {
-					for(int y=0; y<currentLayer.getWidth(); y++) {
-						TiledMapTile tile = currentLayer.getCell(x, y).getTile();
-						if(tile.getId()==3) {
-							finishLineList.getMapList().add(new Position(x,y));
-						}else if(tile.getId()==0) {
-							startPos = new Position(x,y);
+				for(int x=0; x<currentLayer.getWidth(); x++) {
+					for(int y=0; y<currentLayer.getHeight(); y++) {
+						if(currentLayer.getCell(x, y) != null) {
+							TiledMapTile tile = currentLayer.getCell(x, y).getTile();
+							if(tile.getProperties().containsKey("FinishLine")) {
+								finishLineList.getMapList().add(new Position(x,currentLayer.getHeight()-(y+1)));
+							}else if(tile.getProperties().containsKey("StartPosition")) {
+								startPos = new Vector2(x,currentLayer.getHeight()-(y+1));
+							}
 						}
 					}
 				}
