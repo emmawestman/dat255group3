f9d7196642e8bab216b51095a1ff8893bb9f86e1
SaraWallander <sarawallander93@gmail.com>
2013-10-03 14:06:45 +0200

    made some new methods for converting floats between pixels and meters, added javadoc and made the structure a bit nicer

--- dat255_group3/src/com/dat255_group3/utils/CoordinateConverter.java
+++ dat255_group3/src/com/dat255_group3/utils/CoordinateConverter.java
@@ -7,22 +7,64 @@ public class CoordinateConverter {
 	private static int ScreenWidth;
 	private static int ScreenHeight;
 	
-	
-	
-	public static Vector2 pixelToMeter(Vector2 pixels){
-		float pixelToMeter = 10f/(float)CoordinateConverter.ScreenWidth;
-		Vector2 meters = new Vector2 ();
-		meters.x = pixels.x*pixelToMeter;
-		meters.y = pixels.y*pixelToMeter;
+	/**
+	 * converts a pixels into meters, 10 meter = long side of the screen
+	 * @param pixels
+	 * @return meters
+	 */
+	public static float pixelToMeter(float pixels){
+		float pixelToMeter = getPixelToMeter();
+		float meters = 0;
+		meters = pixels*pixelToMeter;
 	    return meters;
 	}
-	
-	public static Vector2  meterToPixel(Vector2 meters){
-		float meterToPixel=(float)CoordinateConverter.ScreenWidth/10f;
-		Vector2 pixels = new Vector2 ();
-		pixels.x = meters.x*meterToPixel;
-		pixels.y = meters.y*meterToPixel;
+	/**
+	 * converts a set of pixels to meters, 10 meter = long side of the screen
+	 * @param pixels
+	 * @return meters
+	 */
+	public static Vector2 pixelToMeter(Vector2 pixels){
+		Vector2 meters = new Vector2 ();
+		meters.x =pixelToMeter(pixels.x);
+		meters.y = pixelToMeter(pixels.y);
+	    return meters;
+	}
+	/**
+	 * converts meters into pixels, 10 meter = long side of the screen
+	 * @param meters
+	 * @return pixels
+	 */
+	public static float  meterToPixel(float meters){
+		float meterToPixel = getMeterToPixel();
+		float pixels = 0;
+		pixels = meters*meterToPixel;
 	    return pixels;
+	}
+	/**
+	 * converts a set of meters into pixels, 10 meter = long side of the screen
+	 * @param meters
+	 * @return pixels
+	 */
+	public static Vector2  meterToPixel(Vector2 meters){
+		Vector2 pixels = new Vector2 ();
+		pixels.x = meterToPixel(meters.x);
+		pixels.y = meterToPixel(meters.y);
+	    return pixels;
+	}
+	
+	/**
+	 * get the number of meters in 1 pixel
+	 * @return amount
+	 */
+	public static float getPixelToMeter(){
+		return 10f/(float)CoordinateConverter.ScreenWidth;
+	}
+	/**
+	 * get the number of pixels in 1 meter
+	 * @return amount
+	 */
+	public static float getMeterToPixel(){
+		return (float)CoordinateConverter.ScreenWidth/10f;
 	}
 	
 	public static int getScreenWidth() {
