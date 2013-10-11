5d52c7343ce28a75af996353f0d869fe8d13f56e
bbjorn <bbjorn@student.chalmers.se>
2013-10-11 09:39:40 +0200

    Forgot to save before previous commit.

--- dat255_group3/src/com/dat255_group3/utils/GyroUtils.java
+++ dat255_group3/src/com/dat255_group3/utils/GyroUtils.java
@@ -6,22 +6,11 @@ import com.badlogic.gdx.Input.Orientation;
 import com.badlogic.gdx.input.*;
 
 public class GyroUtils {
-	
-	/*
-	public static float getRotation(){
-		//float pitch = Gdx.input.getPitch();
-		float pitch = Gdx.input.getpitch();
-		//float azimuth = Gdx.input.getAzimuth();
+	private static float upHill = 30;
+	private static float downHill = -30;
 		
-		//Gdx.app.log("Viking","Rotation: "+Gdx.input.getRotation()+" | Orienation: "+Gdx.input.getNativeOrientation());
-		return pitch;
-	}
-	*/ 
-	
 	public static void gyroSteering(){
 		float pitch = Gdx.input.getPitch();
-		float upHill = 30;
-		float downHill = -30;
 				
 		if (pitch>upHill){
 			Gdx.app.log("Viking","Going up hill: "+pitch);
@@ -31,21 +20,19 @@ public class GyroUtils {
 			Gdx.app.log("Viking","Staying on level "+pitch);
 		}
 	}
-	
-	/*
-	private static Orientation orientation;
 
-	if (orientation == Input.Orientation.valueOf("Portrait")){
-		upHill = 30;
-		downHill = -30;
-	}else if(orientation == Input.Orientation.valueOf("Landscape")){
-		//These values are untested, and intended to be used for tablet devices.
-		upHill = 120;
-		downHill = 60;
-	}else{
-		orientation = Gdx.input.getNativeOrientation();
+	public void checkDeviceOrientation(){
+
+		Orientation orientation = Gdx.input.getNativeOrientation();
+	
+		if (orientation == Input.Orientation.valueOf("Portrait")){
+			upHill = 30;
+			downHill = -30;
+		}else if(orientation == Input.Orientation.valueOf("Landscape")){
+			//These values are untested, and intended to be used for tablet devices.
+			upHill = 120;
+			downHill = 60;
+		}
+	
 	}
-	*/
-
-	
 }
