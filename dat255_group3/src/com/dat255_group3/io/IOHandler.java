c0434f0ca6060f496cd812818800f160afb6b6d5
Sara Johansson <johanssons21@gmail.com>
2013-10-11 10:20:13 +0200

    Added methods getScore and getTime, which reads the txt file and returns the wanted variables.

--- dat255_group3/src/com/dat255_group3/io/IOHandler.java
+++ dat255_group3/src/com/dat255_group3/io/IOHandler.java
@@ -9,27 +9,48 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 
 public class IOHandler {
 	
+	private static FileHandle handle;
+	private static String [] levelData;
+	
+	public IOHandler() {
+		handle = Gdx.files.local("io/io.txt");
+	}
+	
 	public static void saveScoreNTime(int score, int time, String level){
 		try{
-			FileHandle handle = Gdx.files.internal("io/io.txt");
 			handle.writeString(level + "; Score:" + score + "; Time:" + time + '\n', true);
 			Gdx.app.log("IOHandler", "Name: " + handle.name());
 		} catch (GdxRuntimeException e){
 			Gdx.app.log("IOHandler", "Exception", e);
 		}
-		
 	}
 	
 	public static void readScoreNTime(){
 		try{
-			FileHandle handle = Gdx.files.local("ioHandling.txt");
 			String text = handle.readString();
-			
-			String [] levelOne = text.split(";");		
+			levelData = text.split(";");		
 		} catch (GdxRuntimeException e){
 			Gdx.app.log("IOHandler", "Exception", e);
 		}
-		
 	}
-
+	
+	public static int getScore(String level) {
+		for(int i = 0; i<levelData.length; i++) {
+			if (levelData[i].contains(level)) {
+				String score[] = levelData[i+1].split(":");
+				return Integer.parseInt(score[1].trim());
+			}
+		}
+		return 0;
+	}
+	
+	public static int getTime(String level) {
+		for(int i = 0; i<levelData.length; i++) {
+			if (levelData[i].contains(level)) {
+				String time[] = levelData[i+2].split(":");
+				return Integer.parseInt(time[1].trim());
+			}
+		}
+		return 0;
+	}
 }
