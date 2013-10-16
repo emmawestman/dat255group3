a7524ce7a460b332922d4c226a8045308b2cda6a
SaraWallander <sarawallander93@gmail.com>
2013-10-16 11:10:25 +0200

    javadoc

--- dat255_group3/src/com/dat255_group3/controller/CharacterController.java
+++ dat255_group3/src/com/dat255_group3/controller/CharacterController.java
@@ -3,20 +3,22 @@ package com.dat255_group3.controller;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.dat255_group3.model.Character;
 import com.dat255_group3.view.CharacterView;
-
+/**
+ * The controller class for the character
+ */
 public class CharacterController {
 
 	private WorldController worldController;
 	private Character character;
 	private CharacterView characterView;
-	
+	/**
+	 * The constructor creates a character with the default friction 0
+	 * @param worldController 
+	 * @param camera , the camera used to display everything
+	 */
 	public CharacterController(WorldController worldController, OrthographicCamera camera){
 		this.worldController = worldController;
-		
-		/*
-		 * The character and its view will later on be placed in a map/lists as we will have different characters
-		 */
-		this.character = new Character(0.0);
+		this.character = new Character(0);
 		this.characterView = new CharacterView(character, camera);
 
 	}
@@ -28,13 +30,18 @@ public class CharacterController {
 	public CharacterView getCharacterView() {
 		return characterView;
 	}
-	
+	/**
+	 * Jumps the character body if it does not have a vertical velocity
+	 */
 	public void tryToJump() {
 		if(this.worldController.getCharBody().getLinearVelocity().y == 0) {
 			jump();
 		}
 	}
 	
+	/**
+	 * Jumps the character body
+	 */
 	public void jump(){
 		float impulse = this.worldController.getCharBody().getMass()*5;
 		worldController.getCharBody().applyLinearImpulse(0.0f, impulse, 
