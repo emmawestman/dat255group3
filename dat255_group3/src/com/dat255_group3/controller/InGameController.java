ab75452c7b3a6a61e994e05b17972fb0d5757f73
Julia Friberg <juliafriberg93@gmail.com>
2013-10-17 16:54:29 +0200

    Pausebutton added in game, but wrong image

--- dat255_group3/src/com/dat255_group3/controller/InGameController.java
+++ dat255_group3/src/com/dat255_group3/controller/InGameController.java
@@ -5,10 +5,17 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.dat255_group3.io.IOHandler;
 import com.dat255_group3.model.InGame;
@@ -30,12 +37,15 @@ public class InGameController implements Screen {
 	private Box2DDebugRenderer renderer = new Box2DDebugRenderer(true, true, true, true, true, true);
 	private boolean gameOver;
 	private boolean isCountingDown = true;
+	private Stage stage;
 
 	public InGameController(MyGdxGameController myGdxGameController) {
 		this.myGdxGameController = myGdxGameController;
 		this.cameraController = new OrthographicCameraController();
 		this.cameraController.create();
-		
+		this.stage = new Stage(CoordinateConverter.getCameraWidth(),
+				CoordinateConverter.getCameraHeight(), true);
+
 	}
 
 	@Override
@@ -43,6 +53,9 @@ public class InGameController implements Screen {
 		// Shows a white screen
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		// Update the stage actors
+		stage.act(delta);
 		
 		inGame.setDelayTime(inGame.getDelayTime() + delta);
 
@@ -78,6 +91,9 @@ public class InGameController implements Screen {
 				this.worldController.getCookieController().getCookieView(),
 				this.worldController.getEnemy(),
 				worldController.getWorld().getTime(), worldController.getWorld().getCookieCounter(), gameOver);
+		
+		// Draws the pause button
+		stage.draw();
 
 		/*
 		 * Checks whether the screen has been touched. If so, a method which
@@ -88,21 +104,21 @@ public class InGameController implements Screen {
 		}
 		// Count Down
 		if(isCountingDown) {
-				if (inGame.getDelayTime() <= 1.0) {
-					Gdx.app.log("InGameContoller", "Count down: 3");
-					inGameView.drawCountDownNbr(inGame.getDelayTime());
-					
-				}else if (inGame.getDelayTime() <= 2.0) {
-					inGameView.drawCountDownNbr(inGame.getDelayTime());
-					Gdx.app.log("InGameContoller", "Count down: 2");
-					
-				}else if (inGame.getDelayTime() <= 3.0) {
-					inGameView.drawCountDownNbr(inGame.getDelayTime());
-					Gdx.app.log("InGameContoller", "Count down: 1");
+			if (inGame.getDelayTime() <= 1.0) {
+				Gdx.app.log("InGameContoller", "Count down: 3");
+				inGameView.drawCountDownNbr(inGame.getDelayTime());
 
-				}else {
-					isCountingDown = false;
-				}
+			}else if (inGame.getDelayTime() <= 2.0) {
+				inGameView.drawCountDownNbr(inGame.getDelayTime());
+				Gdx.app.log("InGameContoller", "Count down: 2");
+
+			}else if (inGame.getDelayTime() <= 3.0) {
+				inGameView.drawCountDownNbr(inGame.getDelayTime());
+				Gdx.app.log("InGameContoller", "Count down: 1");
+
+			}else {
+				isCountingDown = false;
+			}
 		}else {
 			update(delta);
 		}
@@ -111,6 +127,28 @@ public class InGameController implements Screen {
 
 	@Override
 	public void show() {
+		Gdx.input.setInputProcessor(stage);
+		
+		// Pause button
+		ImageButtonStyle pauseButtonStyle = new ImageButtonStyle();
+		TextureAtlas pauseAtlas = new TextureAtlas(
+				Gdx.files.internal("ui/music/music.pack"));
+		Skin pauseSkin = new Skin(pauseAtlas);
+		pauseButtonStyle.up = pauseSkin.getDrawable("music.down");
+		pauseButtonStyle.down = pauseSkin.getDrawable("music.pressed");
+		pauseButtonStyle.checked = pauseSkin.getDrawable("music.up");
+		ImageButton pauseButton = new ImageButton(pauseButtonStyle);
+		pauseButton.setBounds(CoordinateConverter.getCameraWidth() - 50, CoordinateConverter.getCameraHeight() - 100, 150, 150);
+//		pauseButton.pad(20);
+		stage.addActor(pauseButton);
+		pauseButton.toggle();
+		pauseButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				InGameController.this.pause();
+			}
+		});
+
 		if (myGdxGameController.getMyGdxGame().getIsGameStarted() == false) {
 			this.cameraController = new OrthographicCameraController();
 			this.cameraController.create();
@@ -125,6 +163,7 @@ public class InGameController implements Screen {
 		isCountingDown = true;
 
 	}
+
 
 	@Override
 	public void resize(int width, int height) {
@@ -235,21 +274,21 @@ public class InGameController implements Screen {
 
 		// Update the position of the death limit
 		worldController.getCharacterController().getCharacter()
-				.moveDeathLimit(inGame.getSpeedP());
+		.moveDeathLimit(inGame.getSpeedP());
 	}
 
 	public void gameOver() {
 		Gdx.app.log("Game over:", gameOver + "");
 
 		myGdxGameController.getMyGdxGame().setIsGameStarted(false);
-		
+
 		//calculate the score
 		this.myGdxGameController.getPlayerController().getPlayer().calculateScore(worldController.getWorld().getTime(),
-						worldController.getWorld().getCookieCounter(), gameOver);
+				worldController.getWorld().getCookieCounter(), gameOver);
 		save();
 		// Change to gameover-screen
 		myGdxGameController.getGameOverScreen().gameOver(this.myGdxGameController.getPlayerController().getPlayer()
-						.getScore(), worldController.getWorld().getTime(), gameOver);
+				.getScore(), worldController.getWorld().getTime(), gameOver);
 		myGdxGameController.setScreen(myGdxGameController.getGameOverScreen());
 		if(MyGdxGameController.soundEffectsOn()) {
 			if(!gameOver) {
