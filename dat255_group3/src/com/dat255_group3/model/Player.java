673182510c179b77943369ab68d2f6cec4b3b32e
SaraWallander <sarawallander93@gmail.com>
2013-10-21 11:47:13 +0200

    chnged the expected time it takes to finish a level

--- dat255_group3/src/com/dat255_group3/model/Player.java
+++ dat255_group3/src/com/dat255_group3/model/Player.java
@@ -76,7 +76,7 @@ public class Player {
 		if(gameLost){
 			this.score = cookies*100;
 		}else {
-			this.score = (int) (Math.pow(20/time, 3.0)*1000 + cookies*100);
+			this.score = (int) (Math.pow(40/time, 3.0)*1000 + cookies*100);
 		}
 	}
 	public void addHighScoresToList(String [] levelData) {
